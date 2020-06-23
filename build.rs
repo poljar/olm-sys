699d399efd1400c38dfe03deb062825bb097e6df
Jan Christian Grünhage <jan.christian@gruenhage.xyz>
2020-06-23 16:11:26 +0200

    support dynamically linking olm
    Signed-off-by: Jan Christian Grünhage <jan.christian@gruenhage.xyz>

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
 use std::process::{Command, Stdio};
 use std::{env, fs, path::PathBuf};
 
+const OLM_LINK_VARIANT_ENV : &'static str = "OLM_LINK_VARIANT";
+
 fn main() {
     let manifest_dir = match env::var_os("CARGO_MANIFEST_DIR") {
         Some(d) => d,
@@ -8,24 +10,33 @@ fn main() {
     };
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
 
-    // path to olm source code
-    let src = PathBuf::from(&manifest_dir).join("olm");
-    // where we will put our built library for static linking
-    let dst = PathBuf::from(&out_path).join("build");
-    let _ = fs::create_dir(&dst);
-    // path to our final libolm file
-    let dst_file = dst.join("libolm.a");
+    let olm_link_variant = env::var(OLM_LINK_VARIANT_ENV).unwrap_or("static".to_string());
 
-    // building libolm as a static lib
-    if !dst_file.exists() {
-        run(Command::new("make").arg("static").current_dir(&src));
-        let _ = fs::copy(&src.join("build/libolm.a"), &dst_file);
+    if olm_link_variant == "static" {
+        // path to olm source code
+        let src = PathBuf::from(&manifest_dir).join("olm");
+        // where we will put our built library for static linking
+        let dst = PathBuf::from(&out_path).join("build");
+        let _ = fs::create_dir(&dst);
+        // path to our final libolm file
+        let dst_file = dst.join("libolm.a");
+
+        // building libolm as a static lib
+        if !dst_file.exists() {
+            run(Command::new("make").arg("static").current_dir(&src));
+            let _ = fs::copy(&src.join("build/libolm.a"), &dst_file);
+        }
+        println!("cargo:rustc-link-search={}", dst.display());
     }
 
-    // Link to olm static library
-    println!("cargo:rustc-link-lib=static=olm");
-    println!("cargo:rustc-link-search={}", dst.display());
-    // Olm still needs libstdc++
+    // Rebuild if link variant changed
+    println!("cargo:rerun-if-env-changed={}", OLM_LINK_VARIANT_ENV);
+
+    // Link to olm library
+    println!("cargo:rustc-link-lib={}=olm", olm_link_variant);
+
+
+    // Olm needs libstdc++
     println!("cargo:rustc-link-lib=stdc++");
 }
 

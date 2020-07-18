19e18932a122dca32ead57660711327126c78432
stoically <stoically@protonmail.com>
2020-07-18 10:33:46 +0200

    Support compiling to the wasm32-unknown-unknown target

--- build.rs
+++ build.rs
@@ -10,6 +10,7 @@ fn main() {
         None => panic!("Unable to read manifest dir"),
     };
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let target = env::var("TARGET").unwrap();
 
     let olm_link_variant = env::var(OLM_LINK_VARIANT_ENV).unwrap_or_else(|_| "static".to_string());
     // When building on docs.rs, this environment variable is set, and since write access outside
@@ -24,6 +25,18 @@ fn main() {
 
     // Skip building and/or linking of libolm for docs.rs.
     if !docs_rs {
+        let (make_target, src_file) = match target.as_ref() {
+            "wasm32-unknown-unknown" => ("wasm", "build/wasm/libolm.a"),
+            _ => {
+                // Olm needs libstdc++
+                if cfg!(not(target_os = "macos")) {
+                    println!("cargo:rustc-link-lib=stdc++");
+                }
+
+                ("static", "build/libolm.a")
+            }
+        };
+
         if olm_link_variant == "static" {
             // path to olm source code
             let src = PathBuf::from(&manifest_dir).join("olm");
@@ -35,8 +48,8 @@ fn main() {
 
             // building libolm as a static lib
             if !dst_file.exists() {
-                run(Command::new("make").arg("static").current_dir(&src));
-                let _ = fs::copy(&src.join("build/libolm.a"), &dst_file);
+                run(Command::new("make").arg(make_target).current_dir(&src));
+                let _ = fs::copy(&src.join(src_file), &dst_file);
             }
             println!("cargo:rustc-link-search={}", dst.display());
         }
@@ -46,11 +59,6 @@ fn main() {
 
         // Link to olm library
         println!("cargo:rustc-link-lib={}=olm", olm_link_variant);
-
-        // Olm needs libstdc++
-        if cfg!(not(target_os = "macos")) {
-            println!("cargo:rustc-link-lib=stdc++");
-        }
     }
 }
 

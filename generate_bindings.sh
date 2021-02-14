d357bc1415739ad8e81415a5699d22948bcedf1e
Johannes Hayeß <jhaye@mailbox.org>
2021-02-14 16:11:59 +0100

    Manually invoke size_t to usize conversion
    v0.57.0 of bindgen doesn't do this conversion automatically anymore.

--- generate_bindings.sh
+++ generate_bindings.sh
@@ -6,5 +6,5 @@
 # and
 # https://rust-lang.github.io/rust-bindgen/command-line-usage.html
 # first.
-bindgen wrapper.h -o bindings.rs -- -I./olm/include
+bindgen --size_t-is-usize wrapper.h -o bindings.rs -- -I./olm/include
 cat bindings_header.rs bindings.rs > src/lib.rs

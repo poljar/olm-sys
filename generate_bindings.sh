4948898a4b1adebafeee5336318fd807006bd6e6
Johannes Hayeß <jhaye@mailbox.org>
2021-02-14 17:04:02 +0100

    Include Readme as crate docstring when generating lib.rs

--- generate_bindings.sh
+++ generate_bindings.sh
@@ -7,4 +7,14 @@
 # https://rust-lang.github.io/rust-bindgen/command-line-usage.html
 # first.
 bindgen --size_t-is-usize wrapper.h -o bindings.rs -- -I./olm/include
-cat bindings_header.rs bindings.rs > src/lib.rs
+
+# Prepend headers and write everything into the final file.
+cat <( awk '{print "// "$0}' copyright_header.txt) \
+    <(printf '\n') \
+    <( awk '{print "//! "$0}' README.md | tail -n +3) \
+    <(printf '\n') \
+    bindings_header.rs \
+    bindings.rs \
+    > src/lib.rs
+
+rustfmt src/lib.rs
